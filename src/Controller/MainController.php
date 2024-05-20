74226a7d19f3a14c82481ce60b78932de90b90c2
Christopher Bwabwa <christopherbwabwa@gmail.com>
2024-05-21 01:02:25 +0200

    adding twig for html part

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,14 +2,26 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route('/')]
     public function homepage()
     {
-        return new Response('<strong>Starshop</strong>, There you go!');
+        $starshipCount = 457;
+        $myShip = [
+            'name' => 'USS LeafyCruiser (NCC-0001)',
+            'class' => 'Garden',
+            'captain' => 'Jean-Luc Pickles',
+            'status' => 'under construction',
+        ];
+
+        return $this->render('main/homepage.html.twig', [
+            'numberofStars' => $starshipCount,
+            'myShip' => $myShip
+        ]);
     }
 }
